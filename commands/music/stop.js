9eb951baff002632dac4cc78ff2e9f3962bccfa9
페헤 <peheagma977@gmail.com>
2026-04-27 03:27:26 +0900

    자동 커밋: commands/music/pause.js commands/music/stop.js

--- commands/music/stop.js
+++ commands/music/stop.js
@@ -4,20 +4,16 @@ const { getQueue } = require('../../utils/MusicPlayer');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('노래정지')
-        .setDescription('재생을 멈추고 대기열을 초기화합니다'),
+        .setDescription('현재 재생을 일시정지합니다'),
 
     async execute(interaction) {
         const queue = getQueue(interaction.guildId);
 
-        if (!queue) {
+        if (!queue?.current) {
             return interaction.reply({ content: '현재 재생 중인 곡이 없습니다.', ephemeral: true });
         }
 
-        queue.songs = [];
-        queue.current = null;
-        queue.stopping = true;
-        queue.player.stop();
-
-        await interaction.reply('⏹️ 재생을 멈추고 대기열을 초기화했습니다.');
+        queue.player.pause();
+        await interaction.reply('⏸️ 일시정지했습니다.');
     },
 };
