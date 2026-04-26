b5f3e6a75bd23e5ace4bb268a4d147d84454cc8a
페헤 <peheagma977@gmail.com>
2026-04-27 03:38:57 +0900

    자동 커밋: utils/permissions.js

--- utils/permissions.js
+++ utils/permissions.js
@@ -1,10 +1,13 @@
-const ADMIN_ROLE_ID = '1251157860340072548';
 const OWNER_ID = '826036359499481109';
+const ADMIN_ROLE_IDS = [
+    '1464055831816437823', // 부관리자
+    '1251157860340072548', // 가이드
+];
 
 const hasPermission = (member) => {
     return member.id === OWNER_ID
         || member.permissions.has('Administrator')
-        || member.roles.cache.has(ADMIN_ROLE_ID);
+        || ADMIN_ROLE_IDS.some(id => member.roles.cache.has(id));
 };
 
 module.exports = { hasPermission };
